0f807c255cf6e619ddb6f26a2f4808386ba6911d
Abhinav <abhinavlugun@yahoo.com>
2025-05-31 21:43:29 +0700

    Added starting files for head and body

---
+++ frontend/todo-app/src/components/Body.tsx
@@ -0,0 +1,5 @@
+export function Body() {
+    return (<>
+        Body
+    </>);
+}
---
+++ frontend/todo-app/src/components/TodoBox.tsx
@@ -0,0 +1,8 @@
+import { Header } from "./Header";
+import { Body } from "./Body";
+export function TodoBox() {
+    return (<>
+        <Header />
+        <Body />
+    </>);
+}
