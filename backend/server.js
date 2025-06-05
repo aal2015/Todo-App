4310198cec12c1b8010c2c6cf9df698e2c84781d
Abhinav <abhinavlugun@yahoo.com>
2025-06-06 01:07:00 +0700

    Added code for connecting express with postgres

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+require('dotenv').config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: process.env.user,
+  host: 'localhost',
+  database: 'todo',
+  password: process.env.password,
+  port: 5432,
+});
+
+pool.connect();
+
+pool.query("Select * from notes", (err, res) => {
+    if (!err) {
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+});
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log("Here");
+    res.send("Hi");
+});
+
+app.listen(3000);
