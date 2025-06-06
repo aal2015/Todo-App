c8e376ff7d880e71f68d7587a001f673bec18ca2
Abhinav <abhinavlugun@yahoo.com>
2025-06-06 16:28:45 +0700

    Added env in frontend to contain the api url from the backend. Added fetch notes code from backend in frontend

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,16 @@
-// require('dotenv').config();
-
-// const Pool = require('pg').Pool;
-// const pool = new Pool({
-//   user: process.env.user,
-//   host: 'localhost',
-//   database: 'todo',
-//   password: process.env.password,
-//   port: 5432,
-// });
-
-// pool.connect();
-
-// pool.query("Select * from notes", (err, res) => {
-//     if (!err) {
-//         console.log(res.rows);
-//     } else {
-//         console.log(err.message);
-//     }
-// });
-
 const db         = require('./queries')
 const express    = require("express");
 const bodyParser = require('body-parser')
+const cors       = require('cors');
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
+app.use(cors());
 
 app.get("/", (req, res) => {
     console.log("Here");
