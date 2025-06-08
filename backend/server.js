507d72c9a03cb84f6b0275993bc85fe57abd828e
Abhinav <abhinavlugun@yahoo.com>
2025-06-08 23:25:25 +0700

    Added code to refresh access token and for logout in backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const db         = require('./queries');
-const express    = require("express");
-const jwt        = require('jsonwebtoken');
+const db = require('./queries');
+const express = require("express");
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const cors       = require('cors');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -15,20 +15,44 @@ app.use(
 );
 app.use(cors());
 
+let refreshTokens = [];
+
 app.get("/", (req, res) => {
-    console.log("Here");
-    res.send("Hi");
+  console.log("Here");
+  res.send("Hi");
+});
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name })
+    res.json({ accessToken: accessToken })
+  })
 });
 
 app.post("/login", (req, res) => {
   // Authenticate User
 
   const username = req.body.username;
-  const user = {name: username};
-  
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({accessToken: accessToken});
-})  
+  const user = { name: username };
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+})
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+}
 
 app.get('/notes', authenticateToken, db.getNotes);
 app.post('/notes', db.addNote);
@@ -41,7 +65,7 @@ function authenticateToken(req, res, next) {
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
-      next();
+    next();
   });
 }
 
